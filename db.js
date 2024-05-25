b9bcf1b25a0c9ea03d70b0e38f2dff277daeb394
Juhi-so <juhisoni7791@gmail.com>
2024-05-26 00:21:28 +0530

    new commit

--- db.js
+++ db.js
@@ -1,15 +1,19 @@
-const mongoose = require('mongoose');
-const mongoURL = 'mongodb://localhost:27017/hotels'
-mongoose.connect(mongoURL , {
-  useNewUrlParser : true,
-  useUnifiedTopology : true,
-})
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+//const mongoURL = 'mongodb://localhost:27017/hotels'
+const mongoURL = process.env.DB_URL;
+
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
-db.on('connected' , ()=>{
-  console.log("connected to mongodb server")
-})
-db.on('disconnected' , ()=>{
-  console.log("disconnected to mongodb server")
-})
+db.on("connected", () => {
+  console.log("connected to mongodb server");
+});
+db.on("disconnected", () => {
+  console.log("disconnected to mongodb server");
+});
 
 module.exports = db;
--- server.js
+++ server.js
@@ -3,23 +3,21 @@ const app = express();
 const db = require("./db");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-
+require("dotenv").config();
 
 app.get("/", function (req, res) {
   res.send("hello world");
 });
 
-
 //import the router files
-const personRoutes = require('./routes/personRoutes') ;
-const menuItemRoutes = require('./routes/menuItemRoutes') ;
+const personRoutes = require("./routes/personRoutes");
+const menuItemRoutes = require("./routes/menuItemRoutes");
 
 //use the routers
-app.use('/person' , personRoutes);
-app.use('/menu' , menuItemRoutes);
+app.use("/person", personRoutes);
+app.use("/menu", menuItemRoutes);
 
-
-
-app.listen(4000, () => {
+const PORT = process.env.PORT ;
+app.listen(PORT , () => {
   console.log("listening on port 4000");
 });
